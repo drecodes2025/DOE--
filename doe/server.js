50929e19adff27746a4112d9b35b3bb6a17ca566
Andre Castro <lessonsbydre@gmail.com>
2024-11-07 11:40:47 -0300

    Merging Math mudanças no Codigo do Danilo

--- doe/server.js
+++ doe/server.js
@@ -7,14 +7,7 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(express.static('frontend'));
 app.use('/api/donations', donationRoutes);
-app.get('/formulario', (req, res) => {
-    res.sendFile(__dirname + '/ultima.html');
-});
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/ultima.html');
-});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
