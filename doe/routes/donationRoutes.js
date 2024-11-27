b743cc9dc62b21f3fb34a7fc20d0396f1d6bbd21
drecodes2025 <160737584+drecodes2025@users.noreply.github.com>
2024-11-27 17:34:53 -0300

    Delete doe directory

--- doe/routes/donationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createDonation } = require('../controllers/donationController');
-const router = express.Router();
-
-router.post('/donate', createDonation);
-
-module.exports = router;
